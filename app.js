205b7d40818d1bfdb847b718b06a9da9a3a2bf0d
Dev Brainiac <88845813+devbrainiac@users.noreply.github.com>
2024-06-30 14:57:21 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Import WalletConnect modules
-import { Core } from './node_modules/@walletconnect/core/dist/index.esm.min.js';
-import { Web3Wallet } from './node_modules/@walletconnect/web3wallet/dist/index.esm.min.js';
+import { Core } from './node_modules/@walletconnect/core/dist/index.esm.js';
+import { Web3Wallet } from './node_modules/@walletconnect/web3wallet/dist/index.esm.js';
 
 // Create Core instance
 const core = new Core({
